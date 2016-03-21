05571230bcc990c3949c6762b289014a60dcd33b
madcocomo <madcocomo@gmail.com>
2016-03-21 18:19:00 +0800

    session 3,  refactoring

--- src/dojo/session2/RomanConverter.java
+++ src/dojo/session2/RomanConverter.java
@@ -1,32 +1,32 @@
 package dojo.session2;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class RomanConverter {
 
-    static private HashMap<Integer, String> romanMap =
-    new HashMap<Integer, String>(){{
+    static private Map<Integer, String> romanMap =
+    new LinkedHashMap<Integer, String>(){{
         put(1, "I");
         put(5,"V");
         put(10,"X");
         put(50,"L");
+        put(100,"C");
     }};
 
     public String doConvert(int passedNumber) {
         int biggestKey = getBiggestKey(passedNumber);
-        String roman = romanMap.get(passedNumber);
-        if(roman == null) {
-            roman = "";
-            roman += romanMap.get(biggestKey);
-            roman += doConvert(passedNumber - biggestKey);
+
+        if(romanMap.get(passedNumber) == null) {
+            return romanMap.get(biggestKey) + doConvert(passedNumber - biggestKey);
         }
-        return roman;
+        return romanMap.get(biggestKey);
     }
 
     private int getBiggestKey(int passedNumber) {
         int biggestKey = 0;
         for (Integer key : romanMap.keySet())
-            if (key < passedNumber) {
+            if (key <= passedNumber) {
                 biggestKey = key;
             }
         return biggestKey;
