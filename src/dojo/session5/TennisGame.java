844bd92d9472556e0e042f691ca1e37ad7a26c34
madcocomo <madcocomo@gmail.com>
2016-05-30 18:02:56 +0800

    Shotgun Surgery

---
+++ src/dojo/session5/TennisGame.java
@@ -0,0 +1,6 @@
+package dojo.session5;
+
+public interface TennisGame {
+    void wonPoint(String playerName);
+    String getScore();
+}
---
+++ src/dojo/session7/TennisGame.java
@@ -0,0 +1,7 @@
+package dojo.session7;
+
+public interface TennisGame {
+    void wonPoint(String playerName);
+    String getScore();
+}
+
