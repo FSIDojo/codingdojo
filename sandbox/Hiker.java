84a1d4a3f38a93ab4a61e9fc4069a23a70579b44
porcupine_D34D16BCA0 <porcupine@cyber-dojo.org>
2016-10-18 09:46:03 +0000

    13

--- sandbox/Hiker.java
+++ sandbox/Hiker.java
@@ -8,6 +8,9 @@ public class Hiker {
             String[] rs = args.split(",");
             int sum = 0;
             for (int i=0; i<rs.length; i++) {
+                if ("".equals(rs[i])) {
+                    continue;
+                }
                 sum += Integer.parseInt(rs[i]);
             }
             return sum;
