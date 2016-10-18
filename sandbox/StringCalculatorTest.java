6fa5953d2f37be734f787a4588196a06b132fa9a
ray_D34D16BCA0 <ray@cyber-dojo.org>
2016-10-18 09:47:46 +0000

    15

--- sandbox/StringCalculatorTest.java
+++ sandbox/StringCalculatorTest.java
@@ -6,10 +6,18 @@ public class StringCalculatorTest {
     @Test
     public void test_add_with_null() {
         int expected = 0;
-        String numbers = "";
+        String numbers = null;
         int actual = StringCalculator.add(numbers);
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void test_add_with_empty() {
+        int expected = 0;
+        String numbers = "";
+        int actual = StringCalculator.add(numbers);
+        assertEquals(expected, actual);
+    }    
 
     @Test
     public void test_add_with_one() {
