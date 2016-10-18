1391bcf1744f80157b6fe89e385434af2712b673
dolphin_D34D16BCA0 <dolphin@cyber-dojo.org>
2016-10-18 10:39:50 +0000

    52

--- sandbox/StringCal.java
+++ sandbox/StringCal.java
@@ -5,9 +5,10 @@ public class StringCal{
         String delimiter = ",";
         if (str.startsWith("//")){
             delimiter = str.split("\n")[0].substring(2);
+            str = str.split("\n");
             return 0;
         }
-        return add(0, str.substring(2).replaceAll("\n",",").split(","));  
+        return add(0, str.replaceAll("\n",delimiter ).split(delimiter ));  
     }
 
     private static int add(int sum, String[] ints) {
