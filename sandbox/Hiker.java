7f96067e4c9adf38d13f539b7450babf1bd950dd
porcupine_D34D16BCA0 <porcupine@cyber-dojo.org>
2016-10-18 09:24:23 +0000

    8

--- sandbox/Hiker.java
+++ sandbox/Hiker.java
@@ -2,7 +2,7 @@
 public class Hiker {
 
     public static int answer(String args) {
-        if(args.length == 1){
+        if(args.length() == 1){
             return Integer.parseInt(args);
         }
         return 0;
