71a9ec72c51de02bd068f282ac128b0316b0c3e5
porcupine_D34D16BCA0 <porcupine@cyber-dojo.org>
2016-10-18 10:14:24 +0000

    29

--- sandbox/Hiker.java
+++ sandbox/Hiker.java
@@ -1,7 +1,7 @@
 
 public class Hiker {
 
-    private static String split2(String token) {
+    private static String convert(String token) {
         StringBuffer sb = new StringBuffer(256);
         for (int i = 0; i < token.length(); i++) {
              if (token.charAt(i) >= '0' && token.charAt(i) <= '9') {
@@ -15,7 +15,7 @@ public class Hiker {
     
     public static int answer(String args) {
         int sum = 0;
-        String[] rs = split2(args).split(",");
+        String[] rs = convert(args).split(",");
         for (int i=0; i< rs.length; i++) {
             sum = sum + (rs[i].equals("") ? 0 : Integer.parseInt(rs[i]));
         }
