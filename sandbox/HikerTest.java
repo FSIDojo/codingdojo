91f511718ee1d167d2b0e3059c0e0eda9d4e7adb
dolphin_D34D16BCA0 <dolphin@cyber-dojo.org>
2016-10-18 09:16:52 +0000

    2

--- sandbox/HikerTest.java
+++ sandbox/HikerTest.java
@@ -5,7 +5,7 @@ public class HikerTest {
 
     @Test
     public void life_the_universe_and_everything() {
-        int expected = 42;
+        int expected = 0;
         int actual = Hiker.answer();
         assertEquals(expected, actual);
     }
