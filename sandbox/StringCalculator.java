d0c1c5881164a53ba8ae3e1cc460b5fb4afe2ceb
ray_D34D16BCA0 <ray@cyber-dojo.org>
2016-10-18 09:25:39 +0000

    7

--- sandbox/StringCalculator.java
+++ sandbox/StringCalculator.java
@@ -5,6 +5,9 @@ public class StringCalculator {
         if(number == null || number == ""){
             return 0;
         }
+        if(number.split(",").size() == 1){
+            return Integer.valueOf(number);
+        }
         return 6 * 9;
     }
 }
