752163d55d9d7e26ac035754b2dcbdff0d720506
dolphin_D34D16BCA0 <dolphin@cyber-dojo.org>
2016-10-18 10:32:00 +0000

    45

---
+++ sandbox/StringCalTest.java
@@ -0,0 +1,58 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class StringCalTest {
+
+    @Test
+    public void zero() {
+        int expected = 0;
+        int actual = StringCal.answer("");
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void one() {
+        int expected = 1;
+        int actual = StringCal.answer("1");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void two() {
+        int expected = 2;
+        int actual = StringCal.answer("2");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void oneAnd() {
+        int expected = 1;
+        int actual = StringCal.answer("1,");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void spaceAndOne() {
+        int expected = 3;
+        int actual = StringCal.answer("1,2");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void anyStr() {
+        int expected = 10;
+        int actual = StringCal.answer("1,2,3,4");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void newLine() {
+        int expected = 6;
+        int actual = StringCal.answer("1\n2,3");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void dynamicDelimiter() {
+        int expected = 6;
+        int actual = StringCal.answer("//?\n1?2?3");
+        assertEquals(expected, actual);
+    }
+}
