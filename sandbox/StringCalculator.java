e7659db37f08c8d45f921eb6cb80168f08dc83dd
ray_D34D16BCA0 <ray@cyber-dojo.org>
2016-10-18 10:11:00 +0000

    22

--- sandbox/StringCalculator.java
+++ sandbox/StringCalculator.java
@@ -5,17 +5,16 @@ public class StringCalculator {
         if(number == null || "".equals(number)){
             return 0;
         }
-
+        number.replace("\n",",");
         return add(number.split(","));
     }
 
+
     private static int add(String... numbers){
         int ret = 0;
-
         for(String number : numbers){
             ret += Integer.valueOf(number);            
         }
-
         return ret;
     }
 }
