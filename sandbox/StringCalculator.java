362126c30b0afcc73c56fa7fdd396b73f3375caf
ray_D34D16BCA0 <ray@cyber-dojo.org>
2016-10-18 09:22:41 +0000

    5

--- sandbox/StringCalculator.java
+++ sandbox/StringCalculator.java
@@ -2,6 +2,9 @@
 public class StringCalculator {
 
     public static int add(String number) {
+        if(number == null || number == ""){
+            return 0;
+        }
         return 6 * 9;
     }
 }
