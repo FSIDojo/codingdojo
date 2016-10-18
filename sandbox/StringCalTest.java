e726aad4eb34a3af38f47033fa11d78f92e14749
dolphin_D34D16BCA0 <dolphin@cyber-dojo.org>
2016-10-18 10:07:42 +0000

    37

--- sandbox/StringCalTest.java
+++ sandbox/StringCalTest.java
@@ -49,4 +49,11 @@ public class StringCalTest {
         int actual = StringCal.answer("1\n2,3");
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void newLine() {
+        int expected = 6;
+        int actual = StringCal.answer("1\n,2,3");
+        assertEquals(expected, actual);
+    }
 }
