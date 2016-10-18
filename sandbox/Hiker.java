8d3ad100d45622d37a2961ca5fa56935dc81657a
dolphin_D34D16BCA0 <dolphin@cyber-dojo.org>
2016-10-18 09:47:20 +0000

    24

--- sandbox/Hiker.java
+++ sandbox/Hiker.java
@@ -2,13 +2,11 @@
 public class Hiker {
 
     public static int answer(String str) {
-        if(null == str || "".equals(str)) {
-            return 0;
-        }
+
        String[] ints = str.split(",");
-      int sum = 0;
-      for(int i=0; i< ints.length;i++) {
-           sum = sum + Integer.parseInt(ints[i]);
+       int sum = 0;
+       for(int i=0; i< ints.length;i++) {
+           sum = sum + ints[i] == null ? 0:Integer.parseInt(ints[i]);
        }
        return sum;
 
