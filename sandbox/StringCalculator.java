7ab0856aede5e3f0be9381dcd1b087d6b0145d40
ray_D34D16BCA0 <ray@cyber-dojo.org>
2016-10-18 09:28:51 +0000

    12

--- sandbox/StringCalculator.java
+++ sandbox/StringCalculator.java
@@ -5,8 +5,8 @@ public class StringCalculator {
         if(number == null || number == ""){
             return 0;
         }
-        String temp[] = number.split(",").length;
-        if(temp == 1){
+        String temp[] = number.split(",");
+        if(temp.length == 1){
             return Integer.valueOf(temp[0]);
         }else {
             return Integer.valueOf(temp[0]) + Integer.valueOf(temp[1]);
