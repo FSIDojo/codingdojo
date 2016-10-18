37a06c74aede0f0b8cf4a1bdfcb7ad7515cb1d8d
dolphin_D34D16BCA0 <dolphin@cyber-dojo.org>
2016-10-18 10:29:52 +0000

    43

---
+++ sandbox/HikerTest.java
@@ -0,0 +1,39 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class HikerTest {
+
+    @Test
+    public void life_the_universe_and_everything() {
+        int expected = 0;
+        int actual = Hiker.answer(null);
+        assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void one() {
+        int expected = 1;
+        int actual = Hiker.answer("1");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void two() {
+        int expected = 2;
+        int actual = Hiker.answer("2");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void oneAnd() {
+        int expected = 1;
+        int actual = Hiker.answer("1,");
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void spaceAndOne() {
+        int expected = 3;
+        int actual = Hiker.answer("1,2");
+        assertEquals(expected, actual);
+    }
+}
