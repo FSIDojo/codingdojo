5db98cdcc01372362cd5d961bd552d65bac3a400
bee_4361368B07 <bee@cyber-dojo.org>
2016-10-25 10:03:57 +0000

    44

--- sandbox/StringCalculatorTest.java
+++ sandbox/StringCalculatorTest.java
@@ -27,7 +27,7 @@ public class StringCalculatorTest {
 
     @Test
     public void should_accept_newline_seperator() {
-        assertEquals(10, caculator.add("1\n2,7"));
+        assertEquals(10, caculator.add("1\n2"));
     }
     
 }
