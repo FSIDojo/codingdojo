ff2ecf777096b5ce16c29eaea358212e4ba6bde2
bee_4361368B07 <bee@cyber-dojo.org>
2016-10-25 09:59:24 +0000

    42

--- sandbox/StringCalculator.java
+++ sandbox/StringCalculator.java
@@ -3,12 +3,12 @@ public class StringCalculator{
     static final String SEPERATOR = ",";
 
     public int add(String numbers) {
-            String[] numberArray = numbers.split(SEPERATOR);
-            int result = 0;
-            for (String number : numberArray) {
-                result += valueOf(number);
-            }
-            return result;
+        String[] numberArray = numbers.split(SEPERATOR);
+        int result = 0;
+        for (String number : numberArray) {
+            result += valueOf(number);
+        }
+        return result;
     }
 
     private int valueOf(String number) {
