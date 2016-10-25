ed2b37552ee34aeee109828629eff85fad22c138
raccoon_4361368B07 <raccoon@cyber-dojo.org>
2016-10-25 09:20:04 +0000

    5

--- sandbox/StringCalculator.java
+++ sandbox/StringCalculator.java
@@ -2,7 +2,7 @@
 public class StringCalculator {
 
     public static int add(String input) {
-       if(input == ""){ 
+       if(input == null || input.length == 0){ 
            return 0;
        }
        return 0;
--- sandbox/StringCalculatorTest.java
+++ sandbox/StringCalculatorTest.java
@@ -4,6 +4,14 @@ import static org.junit.Assert.*;
 public class StringCalculatorTest {
 
     @Test
+    public void addWithNull() {
+        String input = null;
+        int expected = 0;
+        int actual = StringCalculator.add(input);
+        assertEquals(expected, actual);
+    }
+
+    @Test
     public void addWithEmpty() {
         String input = "";
         int expected = 0;
