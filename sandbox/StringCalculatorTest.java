2bc233d02a71ce92745164c32db51d9fb780f775
bee_4361368B07 <bee@cyber-dojo.org>
2016-10-25 09:21:37 +0000

    7

--- sandbox/StringCalculatorTest.java
+++ sandbox/StringCalculatorTest.java
@@ -2,21 +2,16 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class StringCalculatorTest {
+    StringCalculator caculator = new StringCalculator();
 
     @Test
     public void zero_from_empty() {
-        StringCalculator caculator = new StringCalculator();
-        int expected = 0;
-        int actual = caculator.add("");
-        assertEquals(expected, actual);
+        assertEquals(0, caculator.add(""));
     }
 
      @Test
     public void one_from_one() {
-        StringCalculator caculator = new StringCalculator();
-        int expected = 1;
-        int actual = caculator.add("1");
-        assertEquals(expected, actual);
+        assertEquals(1, caculator.add("1"));
     }
 
 
