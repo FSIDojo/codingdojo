4a9a0afa95bc5b4ecfb4645563234d31568c8c13
raccoon_4361368B07 <raccoon@cyber-dojo.org>
2016-10-25 09:22:29 +0000

    10

--- sandbox/StringCalculatorTest.java
+++ sandbox/StringCalculatorTest.java
@@ -22,7 +22,7 @@ public class StringCalculatorTest {
     @Test
     public void addWithOneInput() {
         String input = "1";
-        int expected = 1
+        int expected = 1;
         int actual = StringCalculator.add(input);
         assertEquals(expected, actual);
     }
