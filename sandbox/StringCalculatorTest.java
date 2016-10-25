a98e196f7d1fb9c3bb4a88cdd25e646db8997240
bee_4361368B07 <bee@cyber-dojo.org>
2016-10-25 09:16:31 +0000

    2

---
+++ sandbox/StringCalculatorTest.java
@@ -0,0 +1,13 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class StringCalculatorTest {
+
+    @Test
+    public void zero_from_empty() {
+        StringCalculator caculator = new StringCalculator();
+        int expected = 0;
+        int actual = caculator.Add("");
+        assertEquals(expected, actual);
+    }
+}
