7fb5995970d5ce5db26d13154b5c193b8a1bc66e
bee_4361368B07 <bee@cyber-dojo.org>
2016-10-25 10:06:55 +0000

    46

--- sandbox/StringCalculator.java
+++ sandbox/StringCalculator.java
@@ -12,8 +12,16 @@ public class StringCalculator{
     }
 
     private int valueOf(String number) {
+        int result = 0;
         if (!"".equals(number)) {
             return Integer.parseInt(number);
+        }
+        if (number.contains("\n")) {
+            String[] numberArray = numbers.split(\n);
+            for (String number : numberArray) {
+                result += valueOf(number);
+            }
+            return result;
         }
         return 0;        
     }
