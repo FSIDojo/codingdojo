f17a6aa2418a0425ff8587a7460be8eeb8a4c8f4
bee_4361368B07 <bee@cyber-dojo.org>
2016-10-25 09:27:03 +0000

    14

--- sandbox/StringCalculator.java
+++ sandbox/StringCalculator.java
@@ -3,7 +3,7 @@ public class StringCalculator{
 
     public static int add(String numbers) {
         if (!"".equals(numbers)) {
-            if (arg.contains(",")) {
+            if (numbers.contains(",")) {
                 return 3;
             }
             return Integer.parseInt(numbers);
