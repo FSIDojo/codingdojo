f919cd8eb6880a7c907a8968908674e8846f11c3
raccoon_4361368B07 <raccoon@cyber-dojo.org>
2016-10-25 09:38:27 +0000

    20

--- sandbox/StringCalculatorTest.java
+++ sandbox/StringCalculatorTest.java
@@ -5,41 +5,31 @@ public class StringCalculatorTest {
 
     @Test
     public void addWithNull() {
-        String input = null;
-        int expected = 0;
-        int actual = StringCalculator.add(input);
-        assertEquals(expected, actual);
+        String input = null; 
+        assertEquals(0, StringCalculator.add(input));
     }
 
     @Test
     public void addWithEmpty() {
         String input = "";
-        int expected = 0;
-        int actual = StringCalculator.add(input);
-        assertEquals(expected, actual);
+        assertEquals(0, StringCalculator.add(input));
     }
 
     @Test
     public void addWithOneInput() {
         String input = "1";
-        int expected = 1;
-        int actual = StringCalculator.add(input);
-        assertEquals(expected, actual);
+        assertEquals(1, StringCalculator.add(input));
     }
 
     @Test
     public void addWithTwoInput() {
         String input = "1,2";
-        int expected = 3;
-        int actual = StringCalculator.add(input);
-        assertEquals(expected, actual);
+        assertEquals(3, StringCalculator.add(input));
     }
 
     @Test
     public void addWithMultipleInput() {
         String input = "1,2,3";
-        int expected = 6;
-        int actual = StringCalculator.add(input);
-        assertEquals(expected, actual);
+        assertEquals(6, StringCalculator.add(input));
     }
 }
