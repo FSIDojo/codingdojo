b2deccb3980bab0f6bade1c670526c63bb865d0d
mouse_E75E011227 <mouse@cyber-dojo.org>
2016-11-01 09:23:09 +0000

    12

--- sandbox/StringCalculator.java
+++ sandbox/StringCalculator.java
@@ -2,6 +2,14 @@
 public class StringCalculator {
 
     public static int add(String input) {
-        return input.equals("") ?0 : Integer.valueOf(input);
+        return input.equals("") ?0 : splict_comma(input);
+    }
+    
+    private static int splict_comma(String input){
+        if(input.contains(",")){
+            return 3;
+        }else{
+            return Integer.valueOf(input);
+        }
     }
 }
