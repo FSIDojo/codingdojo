916dffb492a0ace39eb9717fd99e56a8493f1699
mouse_E75E011227 <mouse@cyber-dojo.org>
2016-11-01 09:26:15 +0000

    20

--- sandbox/StringCalculator.java
+++ sandbox/StringCalculator.java
@@ -10,6 +10,6 @@ public class StringCalculator {
         if(input.contains(",")){
             return Integer.valueOf(parts[0]) + Integer.valueOf(parts[1]);
         }
-        return Integer.valueOf(input);
+        return Integer.valueOf(parts[0]);
     }
 }
