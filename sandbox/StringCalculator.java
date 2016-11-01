67cf6681e645d153550e4d7c3e4af61e40d936d6
mouse_E75E011227 <mouse@cyber-dojo.org>
2016-11-01 09:25:59 +0000

    19

--- sandbox/StringCalculator.java
+++ sandbox/StringCalculator.java
@@ -6,8 +6,8 @@ public class StringCalculator {
     }
     
     private static int splict_comma(String input){
+        String[] parts = input.split(",");
         if(input.contains(",")){
-            String[] parts = input.split(",");
             return Integer.valueOf(parts[0]) + Integer.valueOf(parts[1]);
         }
         return Integer.valueOf(input);
