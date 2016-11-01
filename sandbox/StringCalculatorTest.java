441d0d52cf0310e82a784e58376a95b2709b1aff
mouse_E75E011227 <mouse@cyber-dojo.org>
2016-11-01 09:19:17 +0000

    9

--- sandbox/StringCalculatorTest.java
+++ sandbox/StringCalculatorTest.java
@@ -9,7 +9,9 @@ public class StringCalculatorTest {
     }
 
     @Test
-    public void should_return_one_from_one() {
+    public void should_return_intvalue_from_single_string() {
         assertEquals(1, StringCalculator.add("1"));
+        assertEquals(2, StringCalculator.add("2"));
     }
+
 }
