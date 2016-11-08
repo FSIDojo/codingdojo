a8fb9a58ee0ace7fedeec7fbcfb40a4614148369
gopher_32BD902EC2 <gopher@cyber-dojo.org>
2016-11-08 09:47:23 +0000

    24

--- sandbox/StringCalculator.java
+++ sandbox/StringCalculator.java
@@ -2,14 +2,22 @@
 public class StringCalculator{
 
     public static int add(String numbers) {
+        if ("".equals(numbers)) {
+            return 0;
+        }
+        String [] res = purky(numbers);
+        retrun add(res);
+    }
+    
+    public static String[] purky(String numbers){    
         String splitChar = ",";
         if (numbers.startsWith("//")) {
             splitChar = numbers.charAt(2) + "";
             numbers = numbers.substring(4);
         }
-        return add(numbers.split(splitChar + "|\n"));
-    }
-    
+        return numbers.split(splitChar + "|\n");
+    }        
+
     public static int add(String[] numbers){
         int result = 0;
         for(String temp:numbers){
