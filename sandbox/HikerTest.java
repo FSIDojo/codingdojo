14ac5e92dfa47211441edaea3ca0568c7fcd8797
gopher_32BD902EC2 <gopher@cyber-dojo.org>
2016-11-08 09:13:36 +0000

    2

--- sandbox/HikerTest.java
+++ sandbox/HikerTest.java
@@ -6,7 +6,7 @@ public class HikerTest {
     @Test
     public void oneTwo() {
         int expected = 3;
-        String actual = Hiker.add("1,2")
+        String actual = Hiker.add("1,2");
         assertEquals(expected, actual);
     }
 }
