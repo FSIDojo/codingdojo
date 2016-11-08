c698b2a5ddc46dee75ef066bfcea9fd2652fa477
gopher_32BD902EC2 <gopher@cyber-dojo.org>
2016-11-08 09:44:26 +0000

    21

--- sandbox/StringCalculator.java
+++ sandbox/StringCalculator.java
@@ -8,7 +8,7 @@ public class StringCalculator{
         String splitChar = ",";
         if (numbers.startsWith("//")) {
             splitChar = numbers.charAt(2) + "";
-            numbers = numbers.substring(6);
+            numbers = numbers.substring(4);
         }
         return add(numbers.split(splitChar + "|\n"));
     }
