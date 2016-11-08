b6bc1f65b1741e9fa131981a0a2dcdb600c0f47d
tuna_32BD902EC2 <tuna@cyber-dojo.org>
2016-11-08 09:24:39 +0000

    11

--- sandbox/Hiker.java
+++ sandbox/Hiker.java
@@ -3,14 +3,19 @@ public class Hiker {
 
     public static int answer(String param) {
         String[] splits = purify(param).split(",");
+        
+       return calc(splits);
+        
+    }
+
+    private static String purify(String raw) {
+        return raw.replace("\n",",");
+    }
+    private static int calc(String[] splits) {
         int result = 0;
         for (String s : splits) {
             result += Integer.parseInt(s);
         }
         return result ;
-    }
-
-    private static String purify(String raw) {
-        return raw.replace("\n",",");
     }
 }
