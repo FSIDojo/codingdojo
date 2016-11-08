556e4b66ea0604f26dac66f4359c24201dc29e67
lizard_32BD902EC2 <lizard@cyber-dojo.org>
2016-11-08 09:19:33 +0000

    9

--- sandbox/StringCalculatorTest.java
+++ sandbox/StringCalculatorTest.java
@@ -5,13 +5,11 @@ public class StringCalculatorTest {
 
     @Test
     public void empty_string_get_0() {
-        int actual = new StringCalculator().add("");
-        assertEquals(0, actual);
+        assertEquals(0, new StringCalculator().add(""));
     }
 
     @Test
     public void single_string_get_x() {
-        int actual = new StringCalculator().add("1");
-        assertEquals(1, actual);
+        assertEquals(1, new StringCalculator().add("1"));
     }
 }
