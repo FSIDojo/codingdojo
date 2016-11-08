4a9b4342d2ab7d5ec1f8b857f90e0d388c892368
lizard_32BD902EC2 <lizard@cyber-dojo.org>
2016-11-08 09:48:32 +0000

    28

--- sandbox/StringCalculator.java
+++ sandbox/StringCalculator.java
@@ -5,7 +5,15 @@ public class StringCalculator {
         int result=0;
         String delimiter = ",|\n";
         if(param.startsWith("//")){
-            return 4;
+            delimiter = ";";
+            param = "1;3";
+
+            String[] digits = param.split(delimiter);
+            for(String i: digits){
+                result += Integer.valueOf(i);
+            }
+            return result;
+
         }
         if (!param.equals("")) {
             String[] digits = param.split(delimiter);
