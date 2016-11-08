4028ab5fcbd4a79d0dd6e8345ee36cc873a76daa
lizard_32BD902EC2 <lizard@cyber-dojo.org>
2016-11-08 09:40:22 +0000

    18

--- sandbox/StringCalculator.java
+++ sandbox/StringCalculator.java
@@ -2,16 +2,18 @@
 public class StringCalculator {
 
     public static int add(String param) {
+        String delimiter = ",";
         int result=0;
         if (param.contains("\n")) {
-            String[] digits = param.split("\n");
+            delimiter = "\n";
+            String[] digits = param.split(delimiter);
             for(String i: digits){
                 result += Integer.valueOf(i);
             }            
             return result;
         }
         if (!param.equals("")) {
-            String[] digits = param.split(",");
+            String[] digits = param.split(delimiter);
             for(String i: digits){
                 result += Integer.valueOf(i);
             }
