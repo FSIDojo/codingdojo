98d9aa30e828913c62d91490b58fca8d529d39af
tuna_32BD902EC2 <tuna@cyber-dojo.org>
2016-11-08 09:15:37 +0000

    4

--- sandbox/Hiker.java
+++ sandbox/Hiker.java
@@ -2,6 +2,11 @@
 public class Hiker {
 
     public static int answer(String param) {
-        return 1;
+        String[] splits = param.split(",");
+        int result = 0;
+        for (String s : splits) {
+            result += Integer.parseInt(s);
+        }
+        return result ;
     }
 }
