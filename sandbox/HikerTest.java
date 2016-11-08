8afee8a4beaa2a1fbc91f845c8c14f91fad3c664
tuna_32BD902EC2 <tuna@cyber-dojo.org>
2016-11-08 09:18:33 +0000

    6

--- sandbox/HikerTest.java
+++ sandbox/HikerTest.java
@@ -22,4 +22,11 @@ public class HikerTest {
         int actual = Hiker.answer("1,2,4");
         assertEquals(expected, actual);
     }
+    
+    @Test
+    public void testNewLine() {
+        int expected = 3;
+        int actual = Hiker.answer("1\n2");
+        assertEquals(expected, actual);
+    }
 }
