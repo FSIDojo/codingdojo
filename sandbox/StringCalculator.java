b0e3ec4b82f7606bbdd08f9971188f1a12a48108
lizard_32BD902EC2 <lizard@cyber-dojo.org>
2016-11-08 09:18:21 +0000

    6

--- sandbox/StringCalculator.java
+++ sandbox/StringCalculator.java
@@ -3,7 +3,7 @@ public class StringCalculator {
 
     public static int add(String param) {
         if (param.equals("1")) {
-            return 1;
+            return Integer.valueOf("1");
         }
         return 0;
     }
