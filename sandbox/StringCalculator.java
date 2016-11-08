c9e28dac74ad00955fda4ea041c41c9c13c25a85
gopher_32BD902EC2 <gopher@cyber-dojo.org>
2016-11-08 09:44:51 +0000

    22

--- sandbox/StringCalculator.java
+++ sandbox/StringCalculator.java
@@ -15,9 +15,6 @@ public class StringCalculator{
     
     public static int add(String[] numbers){
         int result = 0;
-        if(numbers.length == 1){
-            return Integer.valueOf(numbers[0]);
-        }
         for(String temp:numbers){
             result += Integer.valueOf(temp);
         }
