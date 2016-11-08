50f2a4babb85c27bd3ff32c55806ca76dc987a37
gopher_32BD902EC2 <gopher@cyber-dojo.org>
2016-11-08 09:19:05 +0000

    9

--- sandbox/StringCalculatorTest.java
+++ sandbox/StringCalculatorTest.java
@@ -6,21 +6,21 @@ public class StringCalculatorTest {
     @Test
     public void space() {
         int expected = 0;
-        int actual = Hiker.add("");
+        int actual = StringCalculator.add("");
         assertEquals(expected, actual);
     }
 
     @Test
     public void one() {
         int expected = 1;
-        int actual = Hiker.add("1");
+        int actual = StringCalculator.add("1");
         assertEquals(expected, actual);
     }
 
     @Test
     public void oneTwo() {
         int expected = 3;
-        int actual = Hiker.add("1,2");
+        int actual = StringCalculator.add("1,2");
         assertEquals(expected, actual);
     }
 
