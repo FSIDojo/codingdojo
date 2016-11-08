84327d54ca04b8d78ac34a05872e6206f0454ab5
tuna_32BD902EC2 <tuna@cyber-dojo.org>
2016-11-08 10:08:28 +0000

    22

--- sandbox/Hiker.java
+++ sandbox/Hiker.java
@@ -14,15 +14,22 @@ public class Hiker {
         } else {
             String[] arry = raw.split("\n");
             String delimiter = arry[0].substring(2);
-            return arry[1].replace(delimiter, ",");
-            
+            return arry[1].replace(delimiter, ",");         
         }
         
     }
     private static int calc(String[] splits) {
         int result = 0;
+        String negatives = "";
         for (String s : splits) {
+            int value = Integer.parseInt(s);
+            if (value < 0) {
+                negatives += value+","; 
+            }
             result += Integer.parseInt(s);
+        }
+        if (negatives.length > 0) {
+            throw new Exception(negatives);
         }
         return result ;
     }
