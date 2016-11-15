bdc68a2eb61b6211dc62f4720b5f63ec5bacf7f9
leopard_B44C744B68 <leopard@cyber-dojo.org>
2016-11-15 10:20:18 +0000

    18

--- sandbox/NumberParser.java
+++ sandbox/NumberParser.java
@@ -4,7 +4,11 @@ public class NumberParser {
         String[] arr = text.split(",");
         int result[] = new int[arr.length];
         for(int i = 0; i < arr.length; i ++) {
-            result[i] = Integer.parseInt(arr[i]);
+            if(arr[i] == null ||"".equals(arr[i])){
+                result[i] = 0;
+            } else {
+                result[i] = Integer.parseInt(arr[i]);
+            }
         }
         
         return result;
