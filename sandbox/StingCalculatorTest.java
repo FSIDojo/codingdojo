11eed53d791ff824ed20d38528eb9533897af877
leopard_B44C744B68 <leopard@cyber-dojo.org>
2016-11-15 09:37:50 +0000

    3

--- sandbox/StingCalculatorTest.java
+++ sandbox/StingCalculatorTest.java
@@ -6,12 +6,15 @@ public class StingCalculatorTest {
 
     @Test
     public void should_parse_number_from_text_and_sum () {
+//given
         NumberParser parser = mock(NumberParser.class);
-        StringCalculator calculator = new StringCalculator(parser);
-        int result = calculator.add("1,2");
-        //verify(parser).parse("1,2");
         int[] a = {1,2};
         when(parser.parse("1,2")).thenReturn(a);
+        StringCalculator calculator = new StringCalculator(parser);
+//when
+        int result = calculator.add("1,2");
+
+//then   
         assertEquals(3, result);
     }
 }
