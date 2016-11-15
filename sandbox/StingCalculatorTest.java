e009ebd3e031fac3bc26dbee5b42bbb27e1578b0
owl_B44C744B68 <owl@cyber-dojo.org>
2016-11-15 09:39:49 +0000

    1

--- sandbox/StingCalculatorTest.java
+++ sandbox/StingCalculatorTest.java
@@ -6,9 +6,13 @@ public class StingCalculatorTest {
 
     @Test
     public void should_parse_number_from_text_and_sum () {
+        //Given
         NumberParser parser = mock(NumberParser.class);
+        when(parse.parse("1,2")).thenReturn(int[]{1,2});
         StringCalculator calculator = new StringCalculator(parser);
-        calculator.add("1,2");
-        verify(parser).parse("1,2");
+        //When
+        int result = calculator.add("1,2");
+        //Then
+        AssertEqual(result, 3);
     }
 }
