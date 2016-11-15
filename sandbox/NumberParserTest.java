50c6b0cdae6a8b41f0c056deccd4441ee64d2e6d
owl_B44C744B68 <owl@cyber-dojo.org>
2016-11-15 10:22:16 +0000

    19

--- sandbox/NumberParserTest.java
+++ sandbox/NumberParserTest.java
@@ -14,4 +14,14 @@ public class NumberParserTest {
         assertArrayEquals(resultArray, new int[0]);
     }
 
+    @Test
+    public void testParser2() {
+        //Given
+        NumberParser parser = new NumberParser();
+        //When
+        int[] resultArray = parser.parse("1,2");
+        //Then
+        assertArrayEquals(resultArray, new int[]{1,2});
+    }
+
 }
