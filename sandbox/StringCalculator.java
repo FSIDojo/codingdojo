025e77266e1612b00ce77fc53a6d2721307be5c3
leopard_B44C744B68 <leopard@cyber-dojo.org>
2016-11-15 10:25:56 +0000

    21

--- sandbox/StringCalculator.java
+++ sandbox/StringCalculator.java
@@ -7,16 +7,12 @@ public class StringCalculator {
     }
 
     public int add(String text) {
-        if (parser != null) {
-           int[] temp = parser.parse(text);
-            
-            int result = 0;
-            for(int a: temp) {
-                result += a;
-            }
-           return result; 
+       int[] temp = parser.parse(text);
+        
+        int result = 0;
+        for(int a: temp) {
+            result += a;
         }
-
-        return 4;
+       return result; 
     }
 }
