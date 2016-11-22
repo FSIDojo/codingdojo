da83aedaf49646f3ca0db9950d168def4e993a58
starfish_87E04C14CE <starfish@cyber-dojo.org>
2016-11-22 09:13:58 +0000

    4

--- sandbox/StingCalculatorTest.java
+++ sandbox/StingCalculatorTest.java
@@ -6,10 +6,14 @@ public class StingCalculatorTest {
 
     @Test
     public void should_parse_number_from_text_and_sum () {
+        //given
         NumberParser parser = mock(NumberParser.class);
         StringCalculator calculator = new StringCalculator(parser);
+        when(parser.parse("1,3")).thenReturn(new int[]{1,3});
+        //when
         int actual = calculator.add("1,3");
+        //then
         verify(parser).parse("1,3");
-        assertEquals(4, actual);
+        assertEquals(1+3, actual);
     }
 }
