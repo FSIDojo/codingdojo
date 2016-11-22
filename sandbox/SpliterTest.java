13f426cad716d09fb721b1551cab8230f249be0b
starfish_87E04C14CE <starfish@cyber-dojo.org>
2016-11-22 10:30:55 +0000

    49

--- sandbox/SpliterTest.java
+++ sandbox/SpliterTest.java
@@ -10,4 +10,11 @@ public class SpliterTest {
         assertArrayEquals(new String[]{"1","2"}, spliter.split("1,2"));
     }
 
+    @Test
+    public void should_split_string_by_2_delimiters() {
+        Spliter spliter = new Spliter();
+        spliter.setDelimiter(",|\n");
+        assertArrayEquals(new String[]{"1","2","3"}, spliter.split("1,2\n3"));
+    }
+
 }
