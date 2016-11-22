da2247f642efc2b2abbec38203b153f9e20d1985
starfish_87E04C14CE <starfish@cyber-dojo.org>
2016-11-22 09:25:54 +0000

    14

--- sandbox/NumberParser.java
+++ sandbox/NumberParser.java
@@ -1,9 +1,6 @@
 
 public class NumberParser {
     public int[] parse(String text) {
-        if(text.equals("")){
-            return new int[]{};
-        }
-        return null;
+        return new int[]{};
     }
 }
