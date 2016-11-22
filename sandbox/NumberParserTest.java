d7959a789c4ff16965bc7a7eac8669936640de31
starfish_87E04C14CE <starfish@cyber-dojo.org>
2016-11-22 10:05:52 +0000

    31

--- sandbox/NumberParserTest.java
+++ sandbox/NumberParserTest.java
@@ -5,21 +5,7 @@ import static org.mockito.Mockito.*;
 public class NumberParserTest {
 
     @Test
-    public void should_return_empty_from_empty_string() {
-        //given
-        Spliter spliter = mock(Spliter.class); 
-        NumberParser parser = new NumberParser(spliter);
-        when(spliter.split("")).thenReturn(new String[]{});
-        //when
-        int[] actual = parser.parse("");
-        //then
-        verify(spliter).setDelimiter(",");
-        verify(spliter).split("");
-        assertArrayEquals(new int[]{}, actual);
-    }
-
-    @Test
-    public void should_return_1_from_1_number() {
+    public void should_convert_string_array_to_int() {
         //given
         Spliter spliter = mock(Spliter.class); 
         NumberParser parser = new NumberParser(spliter);
@@ -31,11 +17,5 @@ public class NumberParserTest {
         verify(spliter).split("1");
         assertArrayEquals(new int[]{1}, actual);
     }
-/*
-    @Test
-    public void should_return_2_from_2_number() {
-        NumberParser parser = new NumberParser();
-        assertArrayEquals(new int[]{2,3}, parser.parse("2,3"));
-    }
-*/
+
 }
