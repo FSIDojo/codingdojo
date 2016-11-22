ab9b1a80639f57889832c9bb5175b661b99cb6d6
starfish_87E04C14CE <starfish@cyber-dojo.org>
2016-11-22 09:44:09 +0000

    20

--- sandbox/NumberParserTest.java
+++ sandbox/NumberParserTest.java
@@ -6,10 +6,18 @@ public class NumberParserTest {
 
     @Test
     public void should_return_empty_from_empty_string() {
-        NumberParser parser = new NumberParser();
-        assertArrayEquals(new int[]{}, parser.parse(""));
+        //given
+        Spliter spliter = mock(Spliter.class); 
+        NumberParser parser = new NumberParser(spliter);
+        when(spliter.split("")).thenReturn(new String[]{});
+        //when
+        int[] actual = parser.parse("");
+        //then
+        varify(spliter).setDelimiter(",");
+        varify(spliter).split("");
+        assertArrayEquals(new int[]{}, actual);
     }
-
+/*
     @Test
     public void should_return_1_from_1_number() {
         NumberParser parser = new NumberParser();
@@ -21,5 +29,5 @@ public class NumberParserTest {
         NumberParser parser = new NumberParser();
         assertArrayEquals(new int[]{2,3}, parser.parse("2,3"));
     }
-
+*/
 }
