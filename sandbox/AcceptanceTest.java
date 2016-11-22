282a7dafa7e8ae2797eb98057875ae2eddf94562
starfish_87E04C14CE <starfish@cyber-dojo.org>
2016-11-22 10:16:55 +0000

    38

--- sandbox/AcceptanceTest.java
+++ sandbox/AcceptanceTest.java
@@ -2,13 +2,19 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class AcceptanceTest {
+    private Spliter spliter = new Spliter();
+    private NumberParser parser = new NumberParser(spliter);
+    private StringCalculator calculator = new StringCalculator(parser);
 
     @Test
     public void shoud_add_numbers_in_string() {
-        Spliter spliter = new Spliter();
-        NumberParser parser = new NumberParser(spliter);
-        StringCalculator calculator = new StringCalculator(parser);
         assertEquals(3, calculator.add("1,2")); 
+    }
+
+
+    @Test
+    public void shoud_add_numbers_with_newline() {
+        assertEquals(6, calculator.add("1\n2,3")); 
     }
 
 }
