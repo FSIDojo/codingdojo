bb6ee5aa58624b2c72bac6cfed0f8b930b18253e
starfish_87E04C14CE <starfish@cyber-dojo.org>
2016-11-22 09:18:49 +0000

    8

--- sandbox/StringCalculator.java
+++ sandbox/StringCalculator.java
@@ -9,9 +9,9 @@ public class StringCalculator {
     public int add(String text) {
         if (parser != null) {
             int[] temp = parser.parse(text);
-            int result;
+            int result = 0;
             for(int a:temp){
-                result +=a;
+                result += a;
             }
             return result;
         }
