22846d3264655e6c29f97bf2e90bee0ebdbb07bd
starfish_87E04C14CE <starfish@cyber-dojo.org>
2016-11-22 10:00:16 +0000

    26

--- sandbox/NumberParser.java
+++ sandbox/NumberParser.java
@@ -8,9 +8,10 @@ public class NumberParser {
     public int[] parse(String text) {
         spliter.setDelimiter(",");
         if(text.equals("")){
+            String[] source = spliter.split(text);
             return new int[]{};
         }else{
-            String[] source = text.split(",");
+            String[] source = spliter.split(text);
             int[] res = new int[source.length];
             for(int i=0;i<source.length;i++ ){
                 res[i] = Integer.valueOf(source[i]);
