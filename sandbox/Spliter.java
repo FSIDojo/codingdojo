009613a2e1ea16b8610ff8a57feeb6d6a8d4f487
starfish_87E04C14CE <starfish@cyber-dojo.org>
2016-11-22 10:11:42 +0000

    35

--- sandbox/Spliter.java
+++ sandbox/Spliter.java
@@ -1,11 +1,13 @@
 
 public class Spliter {
+    
+    private String delimiter;
 
     public void setDelimiter(String delimiter) {
-
+        this.delimiter = delimiter;
     }
 
     public String[] split(String text) {
-        return null;
+        return text.split(delimiter);
     }
 }
