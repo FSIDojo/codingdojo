3086bc42c96b28e3972074d50c58a2da24b3746c
turtle_2CC8272826 <turtle@cyber-dojo.org>
2016-11-29 10:01:52 +0000

    1

---
+++ sandbox/FtsTranslator.java
@@ -0,0 +1,10 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import cucumber.api.java.en.*;
+
+public class FtsTranslator{
+
+    public static String translate(flag, keywords){
+        return "word*";
+    }
+}
