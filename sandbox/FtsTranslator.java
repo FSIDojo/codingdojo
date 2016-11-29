3b928f7a60f13d52138bf769f8f8f41280bab34d
alligator_2CC8272826 <alligator@cyber-dojo.org>
2016-11-29 10:06:36 +0000

    5

--- sandbox/FtsTranslator.java
+++ sandbox/FtsTranslator.java
@@ -2,6 +2,9 @@
 public class FtsTranslator {
 
     public static String translate(boolean flag, String keywords) {
+        if (keywords.startsWith("*") {
+            return "*word*";
+        }
         return "word*";
     }
 }
