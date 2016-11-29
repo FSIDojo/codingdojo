a0fd2a530f64a1c2f3c481733f0faaabbacd7160
rhino_2CC8272826 <rhino@cyber-dojo.org>
2016-11-29 10:05:52 +0000

    7

--- sandbox/FtsTranslator.java
+++ sandbox/FtsTranslator.java
@@ -1,5 +1,8 @@
 public class FtsTranslator {
     public static String translate (boolean exactMatch, String keywords){
+        if (keywords.endsWith("*") {
+            return keywords;
+        }
         return keywords+"*";
     }
 }
