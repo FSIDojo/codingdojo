03cb5ddbff657448d1e12f4954e9a69134a5a9ac
ray_65BDA3D8CD <ray@cyber-dojo.org>
2016-12-06 10:14:49 +0000

    9

--- sandbox/SearchStepDef.java
+++ sandbox/SearchStepDef.java
@@ -8,24 +8,17 @@ public class SearchStepDef {
 
     @Given("^the text content:$")
     public void theTextContent(List<String> text) throws Throwable {
-        throw new PendingException();
+        
     }
     
 
     @When("^I search <([^>]*)>$")
     public void iSearchIndian(String input) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+       
     }
-        
-    @Then("^the result will be: \"([^\"]*)\"$")
-    public void theResultWillBe(String arg1) throws Throwable {
-        throw new PendingException();
-    }
-
+  
     @Then("^the result will be:$")
     public void theResultWillBe(List<String> result) throws Throwable {
 
-        throw new PendingException();
     } 
 }
