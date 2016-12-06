71066e20dd7d2ecb5b345a47ed91a233c239c020
ray_65BDA3D8CD <ray@cyber-dojo.org>
2016-12-06 03:16:00 +0000

    4

--- sandbox/SearchStepDef.java
+++ sandbox/SearchStepDef.java
@@ -3,17 +3,5 @@ import static org.junit.Assert.*;
 import cucumber.api.java.en.*;
 
 public class SearchStepDef {
-
-    @Given("^the text content:$")
-    public void theTextContent(List<String> text) throws Throwable {
-    }
-    
-    @When("^I input \"([^\"]*)\"$")
-    public void iInput(String words) throws Throwable {
-    }
-    
-    @Then("^the result will be: \"([^\"]*)\"$")
-    public void theResultWillBe(String searchResult) throws Throwable {
-    }
-        
+ 
 }
