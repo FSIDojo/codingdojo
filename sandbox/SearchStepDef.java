22d3881d61df4567e92c110fadf64cc1f5234241
ray_65BDA3D8CD <ray@cyber-dojo.org>
2016-12-06 03:17:50 +0000

    5

--- sandbox/SearchStepDef.java
+++ sandbox/SearchStepDef.java
@@ -1,7 +1,23 @@
+import java.util.*;
 import org.junit.*;
 import static org.junit.Assert.*;
 import cucumber.api.java.en.*;
+import cucumber.api.*;
 
 public class SearchStepDef {
- 
+
+    @Given("^the text content:$")
+    public void theTextContent(List<String> text) throws Throwable {
+        throw new PendingException();
+    }
+    
+    @When("^I input <[^>]*>$")
+    public void iInputFive(String input) throws Throwable {
+        throw new PendingException();
+    }
+    
+    @Then("^the result will be: \"([^\"]*)\"$")
+    public void theResultWillBe(String arg1) throws Throwable {
+        throw new PendingException();
+    } 
 }
